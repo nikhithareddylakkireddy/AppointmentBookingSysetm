aba722a6f3d64ea5ab28df36f839fa45d44f4dec
nikhithareddylakkireddy <nikhithareddylakkireddy@gmail.com>
2026-06-19 22:43:29 +0530

    Fixed database indentation

--- database.py
+++ database.py
@@ -1,37 +1,35 @@
 import sqlite3
 
 def init_db():
-conn = sqlite3.connect("appointment.db")
-c = conn.cursor()
+    conn = sqlite3.connect("appointment.db")
+    c = conn.cursor()
 
-```
-# Users Table
-c.execute("""
-CREATE TABLE IF NOT EXISTS users(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT,
-    password TEXT
-)
-""")
+    # Users Table
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS users(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT,
+        password TEXT
+    )
+    """)
 
-# Appointments Table
-c.execute("""
-CREATE TABLE IF NOT EXISTS appointments(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    appointment_id TEXT,
-    patient TEXT,
-    age INTEGER,
-    gender TEXT,
-    phone TEXT,
-    email TEXT,
-    symptom TEXT,
-    department TEXT,
-    doctor TEXT,
-    date TEXT,
-    time TEXT
-)
-""")
+    # Appointments Table
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS appointments(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        appointment_id TEXT,
+        patient TEXT,
+        age INTEGER,
+        gender TEXT,
+        phone TEXT,
+        email TEXT,
+        symptom TEXT,
+        department TEXT,
+        doctor TEXT,
+        date TEXT,
+        time TEXT
+    )
+    """)
 
-conn.commit()
-conn.close()
-```
+    conn.commit()
+    conn.close()
