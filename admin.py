c71191717876d7366e3ef1258ec1e8103215270b
nikhithareddylakkireddy <nikhithareddylakkireddy@gmail.com>
2026-06-19 23:12:11 +0530

    Fixed admin dashboard

--- admin.py
+++ admin.py
@@ -3,11 +3,11 @@ import pandas as pd
 from appointment import get_appointments
 
 def admin_panel():
+
     st.subheader("📊 Admin Dashboard")
 
     appointments = get_appointments()
 
-    # Total appointments card
     st.metric("Total Appointments", len(appointments))
 
     st.markdown("---")
@@ -16,20 +16,37 @@ def admin_panel():
 
     if appointments:
 
-        # Create DataFrame
         df = pd.DataFrame(
             appointments,
             columns=[
                 "ID",
-                "Patient Name",
+                "Appointment ID",
+                "Patient",
+                "Age",
+                "Gender",
+                "Phone",
+                "Email",
+                "Symptom",
+                "Department",
                 "Doctor",
                 "Date",
                 "Time"
             ]
         )
 
-        # Show table
-        st.dataframe(df, use_container_width=True)
+        st.dataframe(
+            df,
+            use_container_width=True
+        )
+
+        csv = df.to_csv(index=False)
+
+        st.download_button(
+            "📥 Download Appointments CSV",
+            csv,
+            "appointments.csv",
+            "text/csv"
+        )
 
     else:
         st.info("No appointments found.")
